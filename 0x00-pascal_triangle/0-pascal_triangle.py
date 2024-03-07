0ad4a1323b0f71add75e2dc4e70e4e40602df770
eddie9110 <edwinmaregwa@gmail.com>
2024-03-07 09:12:36 -0800

    pystylecode compliant

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module contains function to make a pascal triangle"""
 
+
 def pascal_triangle(n):
     """Creates a pascal triangle"""
     if n <= 0:
@@ -8,8 +9,8 @@ def pascal_triangle(n):
     list_row = []
     for a in range(n):
         tmp_list = []
-        for x in range (a+1):
-            if x==0 or x==a:
+        for x in range(a + 1):
+            if x == 0 or x == a:
                 tmp_list.append(1)
             else:
                 tmp_list.append(list_row[a-1][x-1] + list_row[a-1][x])
