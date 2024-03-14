7f2ca384eb8ae156b993111aafc08b4848eb6fc6
eddie9110 <edwimaregwa@gmail.com>
2024-03-14 14:59:06 +0000

    task 0 added

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 def canUnlockAll(boxes):
     """Function determines if all boxes can be opened"""
-    opened_boxes = [0] # box 0 is already unlocked
+    opened_boxes = [0]  # box 0 is already unlocked
     for box in opened_boxes:
         for key in boxes[box]:
             if key < len(boxes) and key not in opened_boxes:
