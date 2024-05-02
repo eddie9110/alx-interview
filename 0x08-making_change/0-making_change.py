6287cf0e42c236c006ad6b6e5f5e1164023c6d4c
eddie9110 <edwinmaregwa@gmail.com>
2024-05-02 00:04:50 -0700

    task 0 added

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+determine the fewest number of coins needed to 
+meet a given amount of change
+"""
+
+def makeChange(coins, total):
+    if total == 0:
+        return 0
+    if len(coins) == 0 or coins is None:
+        return -1
+
+    balance = total
+    change = 0
+    for coin in sorted(coins, reverse=True):
+        while (balance >= coin and balance % coin >= 0):
+            change += int(balance / coin)
+            balance = balance % coin
+    change = change if balance == 0 else -1
+    return change
+
